d1847992a46bb022a7498b95b3220a61e36e26fd
VladFomenko <vlad.fomenko.ua@gmail.com>
2023-11-24 23:42:48 +0200

    fix vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,19 @@
-import {defineConfig} from 'vite'
-
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: []	
-})
+  plugins: [
+    {
+      name: 'add-defer-attribute', // Ім'я плагіну (можете назвати як захочете)
+      transformIndexHtml(html) { // Функція для трансформації HTML
+        return html.replace(/<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, (match) => {
+          if (match.includes('defer')) {
+            return match; // Якщо defer вже є, пропускаємо
+          } else {
+            return match.replace('<script', '<script defer'); // Додаємо defer, якщо його немає
+          }
+        });
+      },
+    },
+  ],
+});
+
